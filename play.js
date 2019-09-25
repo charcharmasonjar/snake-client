bd7ab938db552d7db5318054de5feb93a3b9ec90
Charlotte Dibb <charlottedibb@Charlottes-MacBook-Air.local>
2019-09-25 15:06:38 -0700

    move code for handleUserInput and setupInput functions to input.js file and import functions instead

--- play.js
+++ play.js
@@ -1,26 +1,9 @@
-// play.js
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', function handleUserInput(data) {
-    if (data === '\u0003') {
-      process.exit();
-    }
-  });
-  return stdin;
-}
-
-//'\u0003'
 setupInput();
 
 
